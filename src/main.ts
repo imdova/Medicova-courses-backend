2fe646d3236ded4674bc50ac1a7b159d0e9f7f82
Mali-98 <mohamed.ahmed159840@gmail.com>
2025-08-18 18:51:54 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,8 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import cookieParser from 'cookie-parser';
 
+//Test Main Build Only
+
 let app: NestExpressApplication; // Change to NestExpressApplication for static serving
 
 // Define your username and password for Swagger access
