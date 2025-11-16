179697af452cd01b3cb430fa1111f5a68c4fd94c
Mali-98 <mohamed.ahmed159840@gmail.com>
2025-11-16 21:37:46 +0200

    make course code check endpoint be public

--- src/app.module.ts
+++ src/app.module.ts
@@ -122,9 +122,9 @@ import { AcademySetting } from './academy/academy-settings/entities/academy-sett
       extra: {
         max: 5
       },
-      ssl: {
-        rejectUnauthorized: false, // ✅ Allow self-signed certs from Cloud SQL
-      },
+      // ssl: {
+      //   rejectUnauthorized: false, // ✅ Allow self-signed certs from Cloud SQL
+      // },
     }),
     MailerModule.forRoot({
       transport: process.env.SMTP_TRANSPORT,
